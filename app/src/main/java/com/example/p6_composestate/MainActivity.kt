a1cea041712afd3db633d57048109d547e9fe0c5
xGRRSNx <rodionraskolnikoff2@gmail.com>
2024-04-07 20:58:55 +0300

    zad7
    1. Запустить проект
    2. Проанализировать код
    3. Изучить Toast
    4. Немного «очеловечить» дизайн: увеличить шрифт, центрировать. Использовать при
    необходимости, row, column, модификаторы, элайнменты
    5. Изменить код реакции на клик. Например, в случае нажатости или ненажатости чекбокса
    выдавать различный текст в Тоасте.
    6. Поработать аналогично с Column

--- app/src/main/java/com/example/p6_composestate/MainActivity.kt
+++ app/src/main/java/com/example/p6_composestate/MainActivity.kt
@@ -2,45 +2,83 @@ package com.example.p6_composestate
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val clicks = remember { mutableStateOf(0) }
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Black),
-                contentAlignment = Alignment.BottomCenter,
+            CheckBoxInDaRow()
+        }
+    }
+}
 
-            ) {
-                Text(
-                    text = clicks.value.toString(),
-                    style = TextStyle(fontSize = 48.sp, color = Color.Black),
-                    modifier = Modifier
-                        .background(color = Color.White)
-                        .padding(16.dp)
-                        .clickable { clicks.value += 10 }
-                )
-            }
+@Composable
+fun CheckBoxInDaRow() {
+    val context = LocalContext.current
+    var checkedState by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Row(
+            modifier = Modifier
+                .width(360.dp)
+                .height(100.dp)
+                .padding(16.dp)
+                .border(1.dp, Color.Black, RoundedCornerShape(20.dp))
+                .clickable {
+                    val message = if (checkedState) "Чекбокс выключен" else "Чекбокс включен"
+                    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+                    checkedState = !checkedState
+                },
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = checkedState,
+                onCheckedChange = { checkedState = it }
+            )
+            Text(
+                text = "Нажимая на строку, регулируешь чекбокс!",
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .fillMaxWidth()
+            )
         }
     }
 }
