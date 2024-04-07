ab2686f5de18e430e1aafcf66700a42ae3bb3d6a
xGRRSNx <rodionraskolnikoff2@gmail.com>
2024-04-07 19:52:19 +0300

    zad2
    fun ClicksCount()

--- app/src/main/java/com/example/p6_composestate/MainActivity.kt
+++ app/src/main/java/com/example/p6_composestate/MainActivity.kt
@@ -3,40 +3,31 @@ package com.example.p6_composestate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val clicks = mutableStateOf(0)
         setContent {
-            HelloContent()
+            ClickCount(i = clicks, lambdaCallback = { clicks.value++ })
         }
     }
 }
 
 
 @Composable
-fun HelloContent() {
-    Column(modifier = Modifier.padding(16.dp)) {
-        var name by remember { mutableStateOf("") }
-        if (name.isNotEmpty()) {
-            Text(
-                text = "Welcome, $name!",
-                modifier = Modifier.padding(bottom = 8.dp),
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
-        OutlinedTextField(value = name, onValueChange = { name = it }, label = { Text("Paste your name here") })
-    }
+fun ClickCount(i: State<Int>, lambdaCallback: () -> Unit) {
+    val clicksCount = i.value
+    Text(
+        text = "Clicks: $clicksCount",
+        modifier = Modifier.clickable(onClick = lambdaCallback),
+        fontSize = 25.sp
+    )
 }
