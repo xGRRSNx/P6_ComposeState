f85ac3b9d261e5c2884acfc9330563fae750f521
xGRRSNx <rodionraskolnikoff2@gmail.com>
2024-04-07 20:29:21 +0300

    zad6
    Реализовать проект из примера 2
    1. Проанализировать код
    2. Немного «очеловечить» дизайн: увеличить шрифт, центрировать, задать фон, цвета.
    Использовать при необходимости дополнительные row, column, box модификаторы.
    3. Изменить код реакции на клик. Например, увеличивать счетчик i на 2 при клике.
    4. Реализовать вариант кода из примера 3 и применить функцию remember. «Очеловечить»
    дизайн.

--- app/src/main/java/com/example/p6_composestate/MainActivity.kt
+++ app/src/main/java/com/example/p6_composestate/MainActivity.kt
@@ -1,33 +1,46 @@
 package com.example.p6_composestate
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val clicks = mutableStateOf(0)
         setContent {
-            ClickCount(i = clicks, lambdaCallback = { clicks.value++ })
+            val clicks = remember { mutableStateOf(0) }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black),
+                contentAlignment = Alignment.BottomCenter,
+
+            ) {
+                Text(
+                    text = clicks.value.toString(),
+                    style = TextStyle(fontSize = 48.sp, color = Color.Black),
+                    modifier = Modifier
+                        .background(color = Color.White)
+                        .padding(16.dp)
+                        .clickable { clicks.value += 10 }
+                )
+            }
         }
     }
-}
-
-
-@Composable
-fun ClickCount(i: State<Int>, lambdaCallback: () -> Unit) {
-    val clicksCount = i.value
-    Text(
-        text = "Clicks: $clicksCount",
-        modifier = Modifier.clickable(onClick = lambdaCallback),
-        fontSize = 25.sp
-    )
 }
